2bf97b4d04621027fadcb1c4e9b238d28046a9dc
Monicatatiana11 <58921913+Monicatatiana11@users.noreply.github.com>
2020-12-04 22:17:24 -0500

    Mi primer ejercicio

---
+++ index.js
@@ -0,0 +1,24 @@
+var day = 0;
+switch (day){
+    case 1:
+        document.write ("la bomba es una bomba de agua");
+        break;
+    case 2
+    document.write ("la bomba es una bomba de gasolina");
+        break;
+    
+    case 3 
+    document.write ("la bomba de agua es una bomba de de hormigon");
+         break;
+         
+    case 4 
+    document.write ("la bomba es una bomba de pasta alimenticia");
+    break;
+
+    default
+    document.write ("no existe un valor valido para tipo de bomba");
+
+    //outpusts " Mi ejercicio" //
+
+}
+
